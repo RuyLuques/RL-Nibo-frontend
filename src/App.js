657606bbc2a535aa5081c0b0806b0e9fb1e1ccbe
Ana Ruy Luques <81923419+RuyLuques@users.noreply.github.com>
2023-11-03 17:54:17 -0300

    Configure route homepage
    removed unnecessary components, modified the routes because they need to be in App.js, removed background from all sass components, rendered the HomePage pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import './App.css';
-import Home from './pages/Home'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import HomePage from './pages/HomePage';
+import Footer from './components/Footer/Footer';
+import About from './components/About/About';
+import Projects from './components/Projects/Projects';
+import Seo from './components/SEO/Seo';
+import LandingPage from './components/LandingPage/LandingPage';
+import Automation from './components/Automation/Automation';
+
 function App() {
   return (
-    <div className="App">
-    <Home/>
-    </div>
+    <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/seo" element={<Seo />} />
+          <Route path="/landingPage" element={<LandingPage />} />
+          <Route path="/Automation" element={<Automation />} />
+        </Routes>
+        <Footer />
+    </Router>
   );
 }
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import './Navbar.sass';
+import { Link } from 'react-router-dom';
 
-function Navbar () {
-    return (
-        <div>
-            <h1>Navbar</h1>
+const Navbar = () => {
+  return (
+    <div className="bg-gray-100">
+      <header className="bg-blue-500 p-4">
+        <div className="container mx-auto flex justify-between items-center">
+          <div className="text-white text-2xl font-bold">Logo</div>
+          <nav>
+            <ul className="flex space-x-4">
+              <li><Link to="/" className="text-white hover:text-gray-300" href="#">Home</Link></li>
+              <li><Link to="/about"  className="text-white hover:text-gray-300" href="#">About</Link></li>
+              <li><Link to="/projects"  className="text-white hover:text-gray-300" href="#">projects</Link></li>
+              <li><Link to="/seo"  className="text-white hover:text-gray-300" href="#">seo</Link></li>
+              <li><Link to="/landingPage"  className="text-white hover:text-gray-300" href="#">landingPage</Link></li>
+              <li><Link to="/automation"  className="text-white hover:text-gray-300" href="#">automation</Link></li>
+            </ul>
+          </nav>
         </div>
-    );
+      </header>
+    </div>
+  );
 };
 
 export default Navbar;
