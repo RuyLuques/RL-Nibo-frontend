31b66cc0c5d074809646082549db191cb3e2ae9f
Ana Ruy Luques <81923419+RuyLuques@users.noreply.github.com>
2023-11-03 19:53:39 -0300

    Design
    I created the design of the navbar component, installed font-awesome to use icons and font in the links, it's responsive

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,25 +1,105 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { MenuIcon, XIcon } from '@heroicons/react/solid';
+import 'font-awesome/css/font-awesome.min.css';
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <div className="bg-gray-100">
-      <header className="bg-blue-500 p-4">
+    <>
+      <header className="bg-indigo-400 p-4">
         <div className="container mx-auto flex justify-between items-center">
           <div className="text-white text-2xl font-bold">Logo</div>
-          <nav>
-            <ul className="flex space-x-4">
-              <li><Link to="/" className="text-white hover:text-gray-300" href="#">Home</Link></li>
-              <li><Link to="/about"  className="text-white hover:text-gray-300" href="#">About</Link></li>
-              <li><Link to="/projects"  className="text-white hover:text-gray-300" href="#">projects</Link></li>
-              <li><Link to="/seo"  className="text-white hover:text-gray-300" href="#">seo</Link></li>
-              <li><Link to="/landingPage"  className="text-white hover:text-gray-300" href="#">landingPage</Link></li>
-              <li><Link to="/automation"  className="text-white hover:text-gray-300" href="#">automation</Link></li>
+          <div className="md:hidden flex ml-auto">
+            <button
+              onClick={toggleMenu}
+              className="text-sm text-white hover:text-gray-300"
+            >
+              {menuOpen ? (
+                <XIcon className="h-6 w-6" />
+              ) : (
+                <MenuIcon className="h-6 w-6" />
+              )}
+            </button>
+          </div>
+          <div className="md:flex space-x-4">
+            <ul className="hidden md:flex space-x-4">
+              <li>
+                <Link to="/" className="text-sm front-bold text-white hover:font-bold">
+                  <i className="fa fa-home text-indigo-800"></i> HOME
+                </Link>
+              </li>
+              <li>
+                <Link to="/about" className="text-sm text-white hover:font-bold">
+                  <i className="fa fa-info-circle text-indigo-800"></i> ABOUT
+                </Link>
+              </li>
+              <li>
+                <Link to="/projects" className="text-sm text-white hover:font-bold">
+                  <i className="fa fa-briefcase text-indigo-800"></i> PROJECTS
+                </Link>
+              </li>
+              <li>
+                <Link to="/seo" className="text-sm text-white hover:font-bold">
+                  <i className="fa fa-search text-indigo-800"></i> SEO
+                </Link>
+              </li>
+              <li>
+                <Link to="/landingPage" className="text-sm text-white hover:font-bold">
+                  <i className="fa fa-rocket text-indigo-800"></i> LANDING PAGE
+                </Link>
+              </li>
+              <li>
+                <Link to="/automation" className="text-sm text-white hover:font-bold">
+                  <i className="fa fa-cogs text-indigo-800"></i> AUTOMATION
+                </Link>
+              </li>
             </ul>
-          </nav>
+          </div>
         </div>
       </header>
-    </div>
+      {menuOpen && (
+        <div className="bg-indigo-400 p-4 mx-auto md:hidden">
+          <ul className="p-4 space-y-4">
+            <li>
+                <Link to="/" className="text-sm front-bold text-white hover:font-bold">
+                  <i className="fa fa-home text-indigo-800"></i> HOME
+                </Link>
+            </li>
+            <li>
+              <Link to="/about" className="text-sm front-bold text-white hover:font-bold">
+                  <i className="fa fa-info-circle text-indigo-800"></i> ABOUT
+              </Link>
+            </li>
+            <li>
+              <Link to="/projects" className="text-sm front-bold text-white hover:font-bold">
+                  <i className="fa fa-briefcase text-indigo-800"></i> PROJECTS
+              </Link>
+            </li>
+            <li>
+              <Link to="/seo" className="text-sm front-bold text-white hover:font-bold">
+                  <i className="fa fa-search text-indigo-800"></i> SEO
+              </Link>
+            </li>
+            <li>
+              <Link to="/landingPage" className="text-sm front-bold text-white hover:font-bold">
+                  <i className="fa fa-rocket text-indigo-800"></i> LANDING PAGE
+              </Link>
+            </li>
+            <li>
+              <Link to="/automation" className="text-sm front-bold text-white hover:font-bold">
+                  <i className="fa fa-cogs text-indigo-800"></i> AUTOMATION
+              </Link>
+            </li>
+          </ul>
+        </div>
+      )}
+    </>
   );
 };
 
