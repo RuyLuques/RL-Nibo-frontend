f84e7eec86fdc53e7433cf562411a934889942ec
Ana Ruy Luques <81923419+RuyLuques@users.noreply.github.com>
2023-11-03 19:02:45 -0300

    Design
    I've created a home design that is displayed on the homepage, it's responsive and I've installed heroicons v1

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import './Home.sass';
 
-function Home () {
-    return (
-        <div>
-            <h1>Home</h1>
+const Home = () => {
+  return (
+    <main className="p-5 border-black border">
+      <div className="container mx-auto mt-4 grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div className="bg-white p-4">
+          <h1 className="text-4xl md:text-6xl lg:text-9xl text-3xl md:text-5xl lg:text-7xl text-blue-500 py-5">Welcome</h1>
+          <h2 className="text-3xl md:text-5xl lg:text-7xl text-2xl md:text-4xl lg:text-6xl text-blue-400">I'm Ana Ruy</h2>
+          <h3 className="text-2xl md:text-4xl lg:text-6xl text-xl md:text-3xl lg:text-5xl text-gray-500">Web Developer</h3>
         </div>
-    );
+        <div className="bg-white p-4">
+          <img
+            src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/content-gallery-3.png"
+            alt="Marketing Image 2"
+            className="w-full h-auto rounded-full"
+          />
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Home;
